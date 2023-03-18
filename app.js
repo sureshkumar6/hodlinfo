e4d50247c64cbb1b5f0e36f5cb4779552fc43e29
nac3nt <iamnacent@gmail.com>
2023-03-18 23:22:22 +0530

    proj env setup

--- app.js
+++ app.js
@@ -3,11 +3,22 @@ import cors from "cors";
 import "./db/config.js";
 import Ticker from "./db/ticker.js";
 import ejs from "ejs";
+import path from "path";
+import { fileURLToPath } from 'url';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 const app = express();
+
+// View engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+// App configuration
 app.use(express.json());
 app.use(cors());
-app.set("view engine", "ejs");
+app.use(express.static(path.join(__dirname, 'public')));
+app.use('/css', express.static(path.join(__dirname, 'node_modules', 'bootstrap', 'dist', 'css')));
 
 app.get("/", async (req, resp) => {
   let data = await Ticker.find();
