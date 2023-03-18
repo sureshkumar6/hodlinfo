ae7a6eb57983455358282067b3533e42212dd6d5
nac3nt <iamnacent@gmail.com>
2023-03-19 03:37:11 +0530

    dark theme implemented

--- app.js
+++ app.js
@@ -20,18 +20,34 @@ app.use(cors());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/css', express.static(path.join(__dirname, 'node_modules', 'bootstrap', 'dist', 'css')));
 
+// DB Init
+async function getData(){
+  await Ticker.deleteMany({});
+  const myData= await fetch("https://api.wazirx.com/api/v2/tickers");
+  const resp = await myData.json();
+  let arr = [];
+
+  for (let i in resp){
+      arr.push([i, resp[i]]);
+  }
+  for (let i=0; i< 10; i++){
+      const data= new Ticker({
+          name:arr[i][1]['name'],
+          last: arr[i][1]['last'],
+          buy: arr[i][1]['buy'],
+          sell: arr[i][1]['sell'],
+          volume: arr[i][1]['volume'],
+          base_unit: arr[i][1]['base_unit']
+      })
+      data.save();
+  }
+}
+
+setInterval(getData, 60000);
+
 app.get("/", async (req, resp) => {
   let data = await Ticker.find();
   resp.render("index", { details: data });
-});
-
-app.get("/tickers", async (req, res) => {
-  let data = await Ticker.find();
-  if (data.length > 0) {
-    res.send(data);
-  } else {
-    res.send({ result: "No Data Found" });
-  }
 });
 
 app.listen(3000);
